3a297f7137e83d54fd0bc13ca613f150d69e2062
CC11001100 <CC11001100@qq.com>
2023-08-23 00:44:04 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/scagogogo/sca-base-module-ecosystem-parser
+
+go 1.19
+
+require github.com/scagogogo/sca-base-module-components v0.0.0-20230724024522-a5a359a4003d // indirect
---
+++ pkg/parser/parser.go
@@ -0,0 +1,22 @@
+package base_module_ecosystem_parser
+
+import (
+	"context"
+	"github.com/scagogogo/sca-base-module-components/pkg/models"
+)
+
+// Parser 表示一个解析依赖的Parser
+type Parser[EcosystemDependencyInput, ProjectEcosystem, ModuleEcosystem, ComponentEcosystem, ComponentDependencyEcosystem any] interface {
+
+	// GetName parser的名字，即使是同一个Ecosystem也可能会有多个parser，会根据条件选择parser
+	GetName() string
+
+	// Init 初始化
+	Init(ctx context.Context) error
+
+	// Parse 解析依赖
+	Parse(ctx context.Context, input EcosystemDependencyInput) (*models.Project[ProjectEcosystem, ModuleEcosystem, ComponentEcosystem, ComponentDependencyEcosystem], error)
+
+	// Close 关闭解析器
+	Close(ctx context.Context) error
+}
